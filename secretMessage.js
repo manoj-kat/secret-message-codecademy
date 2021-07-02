0b75fc23a1b09134b14604888847f6357f11791e
manoj kumar <63577869+manoj-kat@users.noreply.github.com>
2021-07-02 23:25:59 +0530

    Create secretMessage.js

---
+++ secretMessage.js
@@ -0,0 +1,19 @@
+// this is rough work with successful excution
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage);
+console.log(secretMessage.indexOf('JavaScript'));
+console.log(secretMessage.length);
+secretMessage.push('to', 'Program');
+console.log(secretMessage);
+console.log(secretMessage.indexOf('easily'));
+secretMessage[7]='right';
+console.log(secretMessage);
+secretMessage.shift();
+console.log(secretMessage);
+secretMessage.unshift('Programming');
+console.log(secretMessage);
+console.log(secretMessage.indexOf('time,'));
+secretMessage.splice(6, 5, 'know,');
+console.log(secretMessage.join(' '));
